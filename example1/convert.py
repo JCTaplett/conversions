ed6a44dc754cf736299c01a14aab081000ad8029
John Taplett <jtaplett@hks.harvard.edu>
2021-01-18 12:28:47 -0500

    update example 1

--- example1/convert.py
+++ example1/convert.py
@@ -1,31 +1,27 @@
-#!/usr/bin/env python3
 
 import csv
 import json
 
-# use csv.DictReader to load input.csv
-# create a variable 'rows' with all of the rows in the csv
+with open('input.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
 
-# loop through each row of the input which is in the format
+json_data = []
+for row in rows:
 
-# {
-#   'title': ..., 
-#   'subtitle': ..., 
-#   'range_min': ..., 
-#   'range_mid': ..., 
-#   'range_max': ..., 
-#   'measure_min': ..., 
-#   'measure_max': ...
-# }
+    item =  {
+        'title': row['title'],
+        'subtitle': row['subtitle'],
+        'ranges': [
+            float(row['range_min']),
+            float(row['range_mid']),
+            float(row['range_max'])],
+        'measures': [
+            float(row['measure_min']),
+            float(row['measure_max'])],
+        'markers': [float(row['markers'])]
+    }
+    json_data.append(item)
 
-# and transform it into the format
-
-# {
-#  'title': ...,
-#  'subtitle': ...,
-#  'ranges': [...],
-#  'measures': [...],
-#  'markers': [...],
-# }
-
-# use json.dump to output, 'output.json'
+with open('output.json', 'w') as jsonfile:
+    json.dump(json_data, jsonfile, indent=4)
