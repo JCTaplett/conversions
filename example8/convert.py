d552433089e6cdc7dd04c06b736ba53331617710
John Taplett <jtaplett@hks.harvard.edu>
2021-01-18 12:47:41 -0500

    update example8

--- example8/convert.py
+++ example8/convert.py
@@ -1,17 +1,30 @@
-#!/usr/bin/env python3
- 
-# load input.csv into a variable `rows`
 
-# parse `modeldate` in each row and sort the `rows` based on `modeldate`
+import csv
+import datetime
 
-# loop through the rows and output a csv, `output.csv` with the date and 6 different series:
+INPUT_DATE_FORMAT = "%m/%d/%Y"
+OUTPUT_DATE_FORMAT = "%Y-%m-%d"
 
-# row['approve_estimate'],
-# row['disapprove_estimate'],
-# row['approve_hi'],
-# row['approve_lo'],
-# row['disapprove_hi'],
-# row['disapprove_lo']
+with open('input.csv') as f:
+    reader = csv.DictReader(f)
+    rows = list(reader)
+    for row in rows:
+        row['modeldate'] = datetime.datetime.strptime(row['modeldate'], INPUT_DATE_FORMAT)
 
-# don't forget the csv header. check dummy.csv
+rows = sorted(rows, key=lambda x: x['modeldate'])
+
+with open('output.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['year','variableA','variableB','variableC','variableD', 'variableE', 'variableF'])
+
+    for row in rows:
+        writer.writerow([
+            row['modeldate'].strftime(OUTPUT_DATE_FORMAT),
+            row['approve_estimate'],
+            row['disapprove_estimate'],
+            row['approve_hi'],
+            row['approve_lo'],
+            row['disapprove_hi'],
+            row['disapprove_lo']
+        ])
 
